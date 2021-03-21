2c09d0f2210fca8d5ac24d87c51d3151c9c43fbd
Júlio César Balbinot <julio.cesar.balbinot@icloud.com>
2021-03-21 04:21:23 -0300

    adding DefaultPageProps

--- types.d.ts
+++ types.d.ts
@@ -1,20 +1,26 @@
 export interface Album {
-    userId: number;
-    id: number;
-    title: string;
+  userId: number;
+  id: number;
+  title: string;
 }
 export type Albums = Array<Album>;
 
 export interface Photo {
-    albumId: number;
-    id: number;
-    title: string;
-    url: string;
-    thumbnailUrl: string;
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 }
 export type Photos = Array<Photo>;
 
 export interface ExtendedAlbum extends Album {
-    thumbnailUrl: string;
-    photos: Photos;
+  thumbnailUrl: string;
+  photos: Photos;
+}
+
+export interface DefaultPageProps {
+  user: null | string;
+  token: null | string;
+  setUser: (value: null | string) => void;
 }
