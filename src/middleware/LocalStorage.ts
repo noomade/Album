1a18cc94b170cc8af7c5095b7427fd3e98b2fbe7
Júlio César Balbinot <julio.cesar.balbinot@icloud.com>
2021-03-22 11:54:15 -0300

    only managing localStorage when on client-side

--- src/middleware/LocalStorage.ts
+++ src/middleware/LocalStorage.ts
@@ -4,27 +4,21 @@ export enum SupportedStorageKeys {
 }
 
 export const save = <T>(key: SupportedStorageKeys, value: T): void => {
-  try {
+  if (typeof window !== 'undefined') {
     const parsedValue = JSON.stringify(value);
     window.localStorage.setItem(key, parsedValue);
-  } catch (e) {
-    //console.log('ERROR save', e);
   }
 };
 
 export const retrieve = <T>(key: SupportedStorageKeys): T => {
-  try {
+  if (typeof window !== 'undefined') {
     const response = window.localStorage.getItem(key);
     return response ? JSON.parse(response) : null;
-  } catch (e) {
-    //console.log('ERROR retrieve', e);
   }
 };
 
 export const removeEntry = (key: SupportedStorageKeys): void => {
-  try {
+  if (typeof window !== 'undefined') {
     window.localStorage.removeItem(key);
-  } catch (e) {
-    //console.log('ERROR removeEntry', e);
   }
 };
