fa1954b874d89d59d79431630fec753c6c42adb2
Júlio César Balbinot <julio.cesar.balbinot@icloud.com>
2021-03-22 11:54:51 -0300

    Adding render return type on _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,12 @@ import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../helpers/theme';
 
 export default class MyDocument extends Document {
-  render() {
+  componentDidCatch(error, errorInfo): void {
+    console.log('error', error);
+    console.log('errorInfo', errorInfo);
+  }
+
+  render(): JSX.Element {
     return (
       <Html lang="en">
         <Head>
